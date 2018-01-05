73d642df1d1c0d6344060feea2cdf44366f2b1d2
Sam Lakerveld <darkwater124@gmail.com>
2018-01-05 01:02:42 +0100

    Rewrite to use relm

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ influence
 
 Runs commands.
 
-Reads bookmarks from `$HOME/.local/share/influence/bookmarks.txt`. Currently crashes if this file doesn't exist.
+Reads bookmarks from `$HOME/.config/influence/bookmarks.txt`.
 
 ![screenshot](https://novaember.com/s/8f9453/bFAXpx.png)
--- src/main.rs
+++ src/main.rs
@@ -1,125 +1,178 @@
-extern crate gtk;
+#![feature(proc_macro)]
 extern crate gdk;
-extern crate gdk_sys;
+extern crate gtk;
+#[macro_use]
+extern crate relm;
+#[macro_use]
+extern crate relm_derive;
 
+use gdk::prelude::*;
+use gtk::Window;
 use gtk::prelude::*;
-use std::env;
+use relm::{Relm, Update, Widget};
 use std::fs::File;
-use std::io;
 use std::io::prelude::*;
-use std::ops::Deref;
-use std::path::Path;
+use std::path::PathBuf;
 use std::process::Command;
-use std::rc::Rc;
 
-macro_rules! clone {
-    (@param _ ) => ( _ );
-    (@param $x:ident) => ( $x );
-    ($($n:ident),+ => move || $body:expr) => (
-        {
-            $( let $n = $n.clone(); )+
-            move || $body
-        }
-    );
-    ($($n:ident),+ => move |$($p:tt),+| $body:expr) => (
-        {
-            $( let $n = $n.clone(); )+
-            move |$(clone!(@param $p),)+| $body
-        }
-    );
-    ($($n:ident),+ => move |$($p:tt : $z:ty),+| $body:expr) => (
-        {
-            $( let $n = $n.clone(); )+
-            move |$(clone!(@param $p) : $z,)+| $body
-        }
-    );
+pub struct Model {
+    bookmarks: Vec<String>,
 }
 
-/// Returns the contents of a file
-fn read_file<P: AsRef<Path>>(path: P) -> io::Result<String> {
+#[derive(Msg)]
+pub enum Msg {
+    Quit,
+}
+
+struct Win {
+    model: Model,
+    window: Window,
+}
+
+impl Update for Win {
+    type Model = Model;
+    type ModelParam = ();
+    type Msg = Msg;
+
+    fn model(_relm: &Relm<Self>, _param: Self::ModelParam) -> Model {
+        let bookmarks = read_bookmarks().unwrap_or_default();
+
+        Model { bookmarks }
+    }
+
+    fn update(&mut self, event: Msg) {
+        match event {
+            Msg::Quit => gtk::main_quit(),
+        }
+    }
+}
+
+impl Widget for Win {
+    type Root = Window;
+
+    fn root(&self) -> Self::Root {
+        self.window.clone()
+    }
+
+    fn view(relm: &Relm<Self>, model: Self::Model) -> Self {
+        let window = gtk::Window::new(gtk::WindowType::Toplevel);
+        window.set_wmclass("influence", "influence");
+        window.set_type_hint(gdk::WindowTypeHint::Dialog);
+        window.set_decorated(false);
+
+        let screen = window.get_screen().unwrap();
+        let monitor_id = screen.get_primary_monitor();
+        let monitor = screen.get_monitor_geometry(monitor_id);
+
+        let padding = 40;
+        let window_width = 600;
+        let window_height = 250;
+        let window_x = monitor.x + padding;
+        let window_y = monitor.y + monitor.height - padding - window_height;
+        window.move_(window_x, window_y);
+        window.set_size_request(window_width, window_height);
+
+        window.set_app_paintable(true);
+        let visual = screen.get_rgba_visual().unwrap();
+        window.set_visual(Some(&visual));
+
+        let container = gtk::Box::new(gtk::Orientation::Vertical, 0);
+        window.add(&container);
+
+        let scroller = gtk::ScrolledWindow::new(None, None);
+        let cmdlist = gtk::ListBox::new();
+        cmdlist.set_vexpand(true);
+        scroller.add(&cmdlist);
+        container.add(&scroller);
+
+        let input = gtk::Entry::new();
+        container.add(&input);
+
+        // input.connect_changed(clone!(cmdlist => move |widget| {
+        //     (*cmdlist).filter(widget.get_text().unwrap_or("".to_string()));
+        // }));
+
+        // input.connect_key_press_event(|input, event| {
+        //     use gdk::enums::key;
+
+        //     match event.get_keyval() {
+        //         key::Return => {
+        //             run_command_and_exit(&input.get_text().unwrap_or("".to_string()));
+        //             Inhibit(true)
+        //         },
+
+        //         key::Escape => {
+        //             gtk::main_quit();
+        //             Inhibit(true)
+        //         }
+
+        //         _ => Inhibit(false)
+        //     }
+        // });
+
+        // let bookmarks = cmdlist.bookmarks.clone();
+        // cmdlist.command_list.connect_key_press_event(clone!(input => move |cmdlist, event| {
+        //     use gdk::enums::key;
+
+        //     match event.get_keyval() {
+        //         key::Tab => {
+        //             let index = cmdlist.get_children().iter().position(|n| n.has_focus()).unwrap();
+        //             let cmd = &bookmarks[index];
+        //             input.set_text(&cmd);
+        //             input.grab_focus_without_selecting();
+        //             input.set_position(-1);
+        //             Inhibit(true)
+        //         },
+
+        //         key::Return => {
+        //             let index = cmdlist.get_children().iter().position(|n| n.has_focus()).unwrap();
+        //             let cmd = &bookmarks[index];
+        //             run_command_and_exit(cmd);
+        //             Inhibit(true)
+        //         },
+
+        //         _ => Inhibit(false)
+        //     }
+        // }));
+
+        connect!(
+            relm,
+            window,
+            connect_delete_event(_, _),
+            return (Some(Msg::Quit), Inhibit(false))
+        );
+
+        window.show_all();
+        input.grab_focus();
+
+        window
+            .get_window()
+            .unwrap()
+            .set_background_rgba(&gdk::RGBA {
+                red: 0x1d as f64 / 255.0,
+                green: 0x1f as f64 / 255.0,
+                blue: 0x21 as f64 / 255.0,
+                alpha: 0xeb as f64 / 255.0,
+            });
+
+        Win { model, window }
+    }
+}
+
+/// Read bookmarks from configuration file
+fn read_bookmarks() -> Result<Vec<String>, Box<std::error::Error>> {
+    let mut path = PathBuf::from(std::env::var("HOME")?);
+    path.push(".config/influence/bookmarks.txt");
+
     let mut string = String::new();
-    let mut file   = File::open(path)?;
-    let _          = file.read_to_string(&mut string)?;
+    let mut file = File::open(path)?;
+    let _ = file.read_to_string(&mut string)?;
+    let bookmarks = string
+        .lines()
+        .map(|l| l.to_string())
+        .collect::<Vec<String>>();
 
-    Ok(string)
-}
-
-struct CommandListItem {
-    widget: Box<gtk::Label>
-}
-
-impl CommandListItem {
-    fn new(cmd: &str) -> Self {
-        let label = gtk::Label::new(Some(cmd));
-        label.set_halign(gtk::Align::Start);
-        label.set_size_request(-1, 25);
-
-        CommandListItem {
-            widget: Box::new(label)
-        }
-    }
-}
-
-impl Deref for CommandListItem {
-    type Target = gtk::Label;
-
-    fn deref(&self) -> &Self::Target {
-        self.widget.deref()
-    }
-}
-
-struct CommandList {
-    widget: gtk::ScrolledWindow,
-    command_list: gtk::ListBox,
-    bookmarks: Vec<String>
-}
-
-impl CommandList {
-    fn new() -> Self {
-        let container = gtk::ScrolledWindow::new(None, None);
-        container.set_vexpand(true);
-
-        let command_list = gtk::ListBox::new();
-        command_list.set_valign(gtk::Align::End);
-        command_list.set_selection_mode(gtk::SelectionMode::None);
-        container.add(&command_list);
-
-        let bookmarks = {
-            let mut bookmark_path = env::home_dir().unwrap();
-            bookmark_path.push(".local/share/influence/bookmarks.txt");
-
-            read_file(bookmark_path).unwrap().lines().map(|l| l.to_string()).rev().collect::<Vec<String>>()
-        };
-
-        for bookmark in &bookmarks {
-            let cmdlist_item = CommandListItem::new(&bookmark);
-            command_list.add(&*cmdlist_item);
-        }
-
-        CommandList {
-            widget: container,
-            command_list: command_list,
-            bookmarks: bookmarks
-        }
-    }
-
-    fn filter(&self, substr: String) {
-        for (i, bookmark) in self.bookmarks.iter().enumerate() {
-            let row = self.command_list.get_row_at_index(i as i32).unwrap();
-            match bookmark.contains(&substr) {
-                true  => row.show(),
-                false => row.hide()
-            }
-        }
-    }
-}
-
-impl Deref for CommandList{
-    type Target = gtk::ScrolledWindow;
-
-    fn deref(&self) -> &Self::Target {
-        &self.widget
-    }
+    Ok(bookmarks)
 }
 
 fn run_command_and_exit(cmd: &str) {
@@ -129,131 +182,32 @@ fn run_command_and_exit(cmd: &str) {
         .spawn()
         .expect("failed to execute child");
 
-    gtk::main_quit();
-}
-
-fn show_window() {
-    if gtk::init().is_err() {
-        println!("Failed to initialize GTK.");
-        return;
-    }
-
-    let window = gtk::Window::new(gtk::WindowType::Toplevel);
-    window.set_name("influence");
-    window.set_type_hint(gdk::WindowTypeHint::Dialog);
-    window.set_decorated(false);
-
-    let screen = window.get_screen().unwrap();
-    let monitor_id = screen.get_primary_monitor();
-    let monitor = screen.get_monitor_geometry(monitor_id);
-
-    let padding = 40;
-    let window_width = 600;
-    let window_height = 250;
-    let window_x = monitor.x + padding;
-    let window_y = monitor.y + monitor.height - padding - window_height;
-
-    window.set_app_paintable(true);
-    let visual = screen.get_rgba_visual().unwrap();
-    window.set_visual(Some(&visual));
-
-    let css_provider = gtk::CssProvider::new();
-    let _ = css_provider.load_from_data("
-    label {
-        font-family: 'Droid Sans Mono';
-        font-size: 10pt;
-        padding: 5px 10px;
-    }
-    list > row:focus {
-        background: #215d9c;
-        outline: none;
-    }
-    entry {
-        font-family: 'Droid Sans Mono';
-        padding: 0px 10px;
-        font-size: 10pt;
-    }
-    ");
-    gtk::StyleContext::add_provider_for_screen(&screen, &css_provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
-
-    let container = gtk::Box::new(gtk::Orientation::Vertical, 0);
-    window.add(&container);
-
-    let cmdlist = Rc::new(CommandList::new());
-    container.add(&**cmdlist);
-
-    let input = gtk::Entry::new();
-    container.add(&input);
-
-    input.connect_changed(clone!(cmdlist => move |widget| {
-        (*cmdlist).filter(widget.get_text().unwrap_or("".to_string()));
-    }));
-
-    input.connect_key_press_event(|input, event| {
-        use gdk::enums::key;
-
-        match event.get_keyval() {
-            key::Return => {
-                run_command_and_exit(&input.get_text().unwrap_or("".to_string()));
-                Inhibit(true)
-            },
-
-            key::Escape => {
-                gtk::main_quit();
-                Inhibit(true)
-            }
-
-            _ => Inhibit(false)
-        }
-    });
-
-    let bookmarks = cmdlist.bookmarks.clone();
-    cmdlist.command_list.connect_key_press_event(clone!(input => move |cmdlist, event| {
-        use gdk::enums::key;
-
-        match event.get_keyval() {
-            key::Tab => {
-                let index = cmdlist.get_children().iter().position(|n| n.has_focus()).unwrap();
-                let cmd = &bookmarks[index];
-                input.set_text(&cmd);
-                input.grab_focus_without_selecting();
-                input.set_position(-1);
-                Inhibit(true)
-            },
-
-            key::Return => {
-                let index = cmdlist.get_children().iter().position(|n| n.has_focus()).unwrap();
-                let cmd = &bookmarks[index];
-                run_command_and_exit(cmd);
-                Inhibit(true)
-            },
-
-            _ => Inhibit(false)
-        }
-    }));
-
-    window.show_all();
-
-    input.grab_focus();
-
-    window.move_(window_x, window_y);
-    window.set_size_request(window_width, window_height);
-
-    window.get_window().unwrap().set_background_rgba(&gdk::RGBA {
-        red:   0x1d as f64 / 255.0,
-        green: 0x1f as f64 / 255.0,
-        blue:  0x21 as f64 / 255.0,
-        alpha: 0xeb as f64 / 255.0
-    });
-
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
-
-    gtk::main();
+    // TODO: send Quit message
 }
 
 fn main() {
-    show_window()
+    Win::run(()).unwrap();
 }
+
+// macro_rules! clone {
+//     (@param _ ) => ( _ );
+//     (@param $x:ident) => ( $x );
+//     ($($n:ident),+ => move || $body:expr) => (
+//         {
+//             $( let $n = $n.clone(); )+
+//             move || $body
+//         }
+//     );
+//     ($($n:ident),+ => move |$($p:tt),+| $body:expr) => (
+//         {
+//             $( let $n = $n.clone(); )+
+//             move |$(clone!(@param $p),)+| $body
+//         }
+//     );
+//     ($($n:ident),+ => move |$($p:tt : $z:ty),+| $body:expr) => (
+//         {
+//             $( let $n = $n.clone(); )+
+//             move |$(clone!(@param $p) : $z,)+| $body
+//         }
+//     );
+// }
