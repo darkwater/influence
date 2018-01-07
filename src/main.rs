f2027e65bb274994f0c617ce4b2367ae75b4e02b
Sam Lakerveld <darkwater124@gmail.com>
2018-01-07 03:30:48 +0100

    Reimplement bookmarks list

--- src/main.rs
+++ src/main.rs
@@ -21,12 +21,16 @@ pub struct Model {
 
 #[derive(Msg)]
 pub enum Msg {
+    Filter(Option<String>),
+    MoveBookmarkSelection(i32),
     Quit,
 }
 
 struct Win {
     model: Model,
     window: Window,
+    bookmarks_listbox: gtk::ListBox,
+    command_entry: gtk::Entry,
 }
 
 impl Update for Win {
@@ -35,14 +39,19 @@ impl Update for Win {
     type Msg = Msg;
 
     fn model(_relm: &Relm<Self>, _param: Self::ModelParam) -> Model {
-        let bookmarks = read_bookmarks().unwrap_or_default();
+        let bookmarks = read_bookmarks().unwrap_or_else(|e| {
+            println!("unable to read bookmarks: {}", e);
+            Default::default()
+        });
 
         Model { bookmarks }
     }
 
-    fn update(&mut self, event: Msg) {
+    fn update(&mut self, event: Self::Msg) {
         match event {
-            Msg::Quit => gtk::main_quit(),
+            Msg::Filter(s)                  => self.filter_bookmarks(s),
+            Msg::MoveBookmarkSelection(dir) => self.move_bookmark_selection(dir),
+            Msg::Quit                       => gtk::main_quit(),
         }
     }
 }
@@ -58,7 +67,9 @@ impl Widget for Win {
         let window = gtk::Window::new(gtk::WindowType::Toplevel);
         window.set_wmclass("influence", "influence");
         window.set_type_hint(gdk::WindowTypeHint::Dialog);
+        window.set_keep_above(true);
         window.set_decorated(false);
+        window.stick();
 
         let screen = window.get_screen().unwrap();
         let monitor_id = screen.get_primary_monitor();
@@ -76,17 +87,58 @@ impl Widget for Win {
         let visual = screen.get_rgba_visual().unwrap();
         window.set_visual(Some(&visual));
 
+        let css_provider = gtk::CssProvider::new();
+        let _ = css_provider.load_from_data(include_bytes!("main.css"));
+        gtk::StyleContext::add_provider_for_screen(&screen, &css_provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
         let container = gtk::Box::new(gtk::Orientation::Vertical, 0);
         window.add(&container);
+
+        // -- Bookmark list --
 
         let scroller = gtk::ScrolledWindow::new(None, None);
         let cmdlist = gtk::ListBox::new();
         cmdlist.set_vexpand(true);
+        cmdlist.set_valign(gtk::Align::End);
         scroller.add(&cmdlist);
         container.add(&scroller);
 
+        for bookmark in &model.bookmarks {
+            let label = gtk::Label::new(Some(bookmark.as_str()));
+            label.set_halign(gtk::Align::Start);
+            label.set_size_request(-1, 25);
+            cmdlist.add(&label);
+        }
+
+        if model.bookmarks.len() > 0 {
+            let last_row = cmdlist.get_row_at_index(model.bookmarks.len() as i32 - 1);
+            cmdlist.select_row(last_row.as_ref());
+        }
+
+        // -- Command input --
+
         let input = gtk::Entry::new();
         container.add(&input);
+
+        connect!(
+            relm,
+            input,
+            connect_changed(widget),
+            Some(Msg::Filter(widget.get_text()))
+        );
+
+        connect!(
+            relm,
+            input,
+            connect_key_press_event(_, key),
+            return {
+                use gdk::enums::key;
+                match key.get_keyval() {
+                    key::Up   => (Some(Msg::MoveBookmarkSelection(-1)), Inhibit(true)),
+                    key::Down => (Some(Msg::MoveBookmarkSelection( 1)), Inhibit(true)),
+                    _         => (None,                                 Inhibit(false)),
+                }
+            }
+        );
 
         // input.connect_changed(clone!(cmdlist => move |widget| {
         //     (*cmdlist).filter(widget.get_text().unwrap_or("".to_string()));
@@ -138,6 +190,19 @@ impl Widget for Win {
         connect!(
             relm,
             window,
+            connect_key_press_event(_, key),
+            return {
+                use gdk::enums::key;
+                match key.get_keyval() {
+                    key::Escape => (Some(Msg::Quit), Inhibit(true)),
+                    _           => (None,            Inhibit(false)),
+                }
+            }
+        );
+
+        connect!(
+            relm,
+            window,
             connect_delete_event(_, _),
             return (Some(Msg::Quit), Inhibit(false))
         );
@@ -155,7 +220,46 @@ impl Widget for Win {
                 alpha: 0xeb as f64 / 255.0,
             });
 
-        Win { model, window }
+        Win {
+            bookmarks_listbox: cmdlist,
+            command_entry: input,
+            model, window
+        }
+    }
+}
+
+impl Win {
+    fn filter_bookmarks(&self, substr: Option<String>) {
+        let substr = substr.unwrap_or_default();
+        for (i, bookmark) in self.model.bookmarks.iter().enumerate() {
+            let row = self.bookmarks_listbox.get_row_at_index(i as i32).unwrap();
+            match bookmark.contains(&substr) {
+                true  => row.show(),
+                false => row.hide(),
+            }
+        }
+
+        // If the current selection went invisible, select the new bottom-most entry
+        let selected = self.bookmarks_listbox.get_selected_row();
+        if selected.is_none() || !selected.unwrap().is_visible() {
+            for index in (0..(self.model.bookmarks.len() as i32 - 1)).rev() {
+                let row = self.bookmarks_listbox.get_row_at_index(index);
+                if let Some(row) = row.and_then(|r| if r.is_visible() { Some(r) } else { None }) {
+                    self.bookmarks_listbox.select_row(Some(&row));
+                    break;
+                }
+            }
+        }
+
+        // Scroll to the bottom
+        if let Some(adj) = self.bookmarks_listbox.get_adjustment() {
+            adj.set_value(adj.get_upper());
+        }
+    }
+
+    fn move_bookmark_selection(&self, dir: i32) {
+        self.bookmarks_listbox.emit_move_cursor(gtk::MovementStep::DisplayLines, dir);
+        self.command_entry.grab_focus_without_selecting();
     }
 }
 
